25b5cabbc55ecbf1feb9bab1ffc3bdc80d05b303
Khrystyna2023 <zagorovsk@gmail.com>
2023-04-05 22:13:45 +0300

    Dz_5: User input 4 integer numbers and program do some math operations with numbers.

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        InputPrint display = new InputPrint();
+        display.Display1();
+    }
+}
+
+
+
+
+
